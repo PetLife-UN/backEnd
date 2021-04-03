c431c80aa35c4885f63d16a2901d3bfe7e380a49
Juan Camilo Villota <jcvillotab@unal.edu.co>
2021-04-03 09:59:14 -0500

    Updating pom.xml in order to disable dependencies not used yet (to get JPA working uncomment spring-boot-starter-data-jpa dependency)

--- src/main/java/com/petlife/backend/petLifeBack/PetLifeBackApplication.java
+++ src/main/java/com/petlife/backend/petLifeBack/PetLifeBackApplication.java
@@ -3,11 +3,14 @@ package com.petlife.backend.petLifeBack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class PetLifeBackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PetLifeBackApplication.class, args);
-	}
+    public static void main(String[] args) {
+    	SpringApplication.run(PetLifeBackApplication.class, args);
+    }
+
+
 
 }
