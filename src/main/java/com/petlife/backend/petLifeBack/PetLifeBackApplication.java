4162fa479fc66e8a2044cb6c63b64eb8221a942a
Juan Camilo Villota <jcvillotab@unal.edu.co>
2021-04-03 07:58:22 -0500

    Setting up Spring Project

---
+++ src/main/java/com/petlife/backend/petLifeBack/PetLifeBackApplication.java
@@ -0,0 +1,13 @@
+package com.petlife.backend.petLifeBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetLifeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetLifeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petlife/backend/petLifeBack/PetLifeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petlife.backend.petLifeBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetLifeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
