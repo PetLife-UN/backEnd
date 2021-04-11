75edd42ec0a6d5a37fe419233740d4b8feb7d763
ogomezs <ogomezs@unal.edu.co>
2021-04-10 22:42:07 -0500

    Add mail send Service
    Agregue una carpeta para guardar la imagen que uso en el correo, pero supongo que es mejor que vaya en la base de datos, pero la imagen es temporal.

---
+++ src/main/java/com/petlife/backend/services/SendEmailService.java
@@ -0,0 +1,36 @@
+package com.petlife.backend.services;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class SendEmailService {
+
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    public void sendEmail(String to, String subject,String token) throws MessagingException {
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        helper.setFrom("noreply@baeldung.com");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(
+                "<h1> Welcome to petday <h1>"+
+                "<h4>Please insert the next code to verify your email account</h4>"+ "<br>" +
+                token+ "<br>" +
+                "<img src='cid:Logo'>"
+                , true
+        );
+        helper.addInline("Logo", new ClassPathResource("public/petlife.jpg"));
+
+        javaMailSender.send(message);
+    }
+}
