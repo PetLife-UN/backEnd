58e443868fde2ad554924d462558777b30d4f8c5
ogomezs <ogomezs@unal.edu.co>
2021-04-11 17:40:10 -0500

    Add online deploy functionality on heroku v.1.0.0

---
+++ src/main/java/com/petlife/backend/services/RoleConfiguration.java
@@ -0,0 +1,27 @@
+package com.petlife.backend.services;
+
+import com.petlife.backend.models.Role;
+import com.petlife.backend.models.eRole;
+import com.petlife.backend.repositories.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+public class RoleConfiguration {
+
+    @Bean
+    CommandLineRunner commandLineRunner(RoleRepository roleRepository){
+        return args -> {
+            eRole user = eRole.ROLE_USER;
+            Role rUser = new Role(user);
+            eRole moderator = eRole.ROLE_MODERATOR;
+            Role rModerator = new Role(moderator);
+            eRole admin = eRole.ROLE_ADMIN;
+            Role rAdmin = new Role(admin);
+            roleRepository.saveAll(List.of(rUser, rModerator, rAdmin));
+        };
+    }
+}
