c2dadcd785885a48698bf46f235142c3a2490ba2
ogomezs <ogomezs@unal.edu.co>
2021-04-12 17:10:19 -0500

    Fix roles creation

--- src/main/java/com/petlife/backend/services/RoleConfiguration.java
+++ src/main/java/com/petlife/backend/services/RoleConfiguration.java
@@ -3,6 +3,7 @@ package com.petlife.backend.services;
 import com.petlife.backend.models.Role;
 import com.petlife.backend.models.eRole;
 import com.petlife.backend.repositories.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,18 +13,24 @@ import java.util.List;
 @Configuration
 public class RoleConfiguration {
 
+    @Autowired
+    RoleRepository roleRepository;
+
     @Bean
     CommandLineRunner commandLineRunner(RoleRepository roleRepository){
         return args -> {
-           /* eRole user = eRole.ROLE_USER;
+
+            eRole user = eRole.ROLE_USER;
             Role rUser = new Role(user);
             eRole moderator = eRole.ROLE_MODERATOR;
             Role rModerator = new Role(moderator);
             eRole admin = eRole.ROLE_ADMIN;
             Role rAdmin = new Role(admin);
-            roleRepository.saveAll(List.of(rUser, rModerator, rAdmin));
-            */
-
+            if(roleRepository.findByName(user).isPresent()){
+                System.out.println("The Roles already extists");
+            }else{
+                roleRepository.saveAll(List.of(rUser, rModerator, rAdmin));
+            }
         };
     }
 }
