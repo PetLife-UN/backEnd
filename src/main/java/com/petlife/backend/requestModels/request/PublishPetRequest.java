b78b324b04d4ba43cfca74e460c179949248cfa9
cmramirezme <65912742+cmramirezme@users.noreply.github.com>
2021-04-23 15:05:34 -0500

    Introduccion de algunos metodos faltantes a PublishPetRequest

--- src/main/java/com/petlife/backend/requestModels/request/PublishPetRequest.java
+++ src/main/java/com/petlife/backend/requestModels/request/PublishPetRequest.java
@@ -43,5 +43,83 @@ public class PublishPetRequest {
     private List<String> links_fotos = new ArrayList<String>();
 
 
+    public String getNombre() {
+        return nombre;
+    }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public eEsteril getEsteril() {
+        return esteril;
+    }
+
+    public void setEsteril(eEsteril esteril) {
+        this.esteril = esteril;
+    }
+
+    public eTipoMascota getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(eTipoMascota tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getRaza() {
+        return raza;
+    }
+
+    public void setRaza(String raza) {
+        this.raza = raza;
+    }
+
+    public eTamaño getTamano() {
+        return tamano;
+    }
+
+    public void setTamano(eTamaño tamano) {
+        this.tamano = tamano;
+    }
+
+    public String getDescripicon() {
+        return descripicon;
+    }
+
+    public void setDescripicon(String descripicon) {
+        this.descripicon = descripicon;
+    }
+
+    public boolean isVacunada() {
+        return vacunada;
+    }
+
+    public void setVacunada(boolean vacunada) {
+        this.vacunada = vacunada;
+    }
+
+    public List<String> getLinks_fotos() {
+        return links_fotos;
+    }
+
+    public void setLinks_fotos(List<String> links_fotos) {
+        this.links_fotos = links_fotos;
+    }
 }
