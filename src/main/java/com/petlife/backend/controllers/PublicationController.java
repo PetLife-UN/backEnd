ba95fb78a5148a20b30e6790a87f56ce54a59476
cmramirezme <65912742+cmramirezme@users.noreply.github.com>
2021-04-23 19:09:58 -0500

    Terminacion PublishController
    Finalizacion de el controlador PublishController

--- src/main/java/com/petlife/backend/controllers/PublicationController.java
+++ src/main/java/com/petlife/backend/controllers/PublicationController.java
@@ -7,33 +7,32 @@ import com.petlife.backend.requestModels.response.MessageResponse;
 import com.petlife.backend.services.PetService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.HashSet;
 import java.util.Set;
 
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/publish")
+
+@PreAuthorize("hasRole('USER') or hasRole('MODERATOR') or hasRole('ADMIN')")
 public class PublicationController {
     @Autowired
     PetService petService;
 
     @PostMapping("/publish-pet")
     public ResponseEntity<?> registerUser(PublishPetRequest publishRequest) {
-        /*
-         *   //debemos validar el token justo aquí??
-         *
-         *   if(validToken(publishRequest.getToken())){
-         *
-         *   };
-         *
-         */
-
 
         // Create new user's account
         Pet pet = new Pet(publishRequest);
-        return ResponseEntity.ok(new MessageResponse("User registered successfully!"));
+        petService.save(pet);
+
+        return ResponseEntity.ok(new MessageResponse("Mascota Publicada Satisfactoriamente"));
 
     }
 
